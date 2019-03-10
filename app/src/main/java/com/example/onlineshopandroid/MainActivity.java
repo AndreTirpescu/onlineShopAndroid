04df3e3e92c35337a15de00630ffd4aa34ef6de1
AndreTirpescu <tirpescu.andrei@yahoo.com>
2019-03-10 14:22:29 +0200

    Added Life cycle support for main activity

--- app/src/main/java/com/example/onlineshopandroid/MainActivity.java
+++ app/src/main/java/com/example/onlineshopandroid/MainActivity.java
@@ -2,16 +2,16 @@ package com.example.onlineshopandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.ListView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
+    static final String MAIN_ACTIVITY_TAG = "[MAIN_ACTIVITY]";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -20,5 +20,40 @@ public class MainActivity extends AppCompatActivity
         ListView productListView = (ListView) findViewById(R.id.products);
         productListView.setAdapter(new ProductTitleAndHypeAdapter(getApplicationContext(), products));
         productListView.setOnItemClickListener(new ProductHypeClickListener(getApplicationContext(), products));
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(MAIN_ACTIVITY_TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(MAIN_ACTIVITY_TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(MAIN_ACTIVITY_TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.i(MAIN_ACTIVITY_TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(MAIN_ACTIVITY_TAG, "onDestroy called");
     }
 }
